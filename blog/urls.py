7047b8d452219345d483499eec74f3fe1fb35f37
Mystic <215104920@qq.com>
2022-05-03 21:48:41 +0800

    feat(update): some django generated info

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 """blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", include("snippets.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
