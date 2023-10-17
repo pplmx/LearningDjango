25dd68b877412f505f285ada2c371a15030143ea
Mystic <215104920@qq.com>
2023-10-17 10:35:29 +0800

    build: add docs for gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,5 +3,8 @@ import multiprocessing
 bind = '0.0.0.0:8000'
 worker_class = 'uvicorn.workers.UvicornWorker'
 
+# https://docs.gunicorn.org/en/stable/settings.html#workers
 workers = multiprocessing.cpu_count() * 2 + 1
+
+# https://docs.gunicorn.org/en/stable/settings.html#threads
 threads = multiprocessing.cpu_count() * 2
