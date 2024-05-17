f6ff835b541ed02b1169eaff74af0a3c959afe1e
Mystic <215104920@qq.com>
2024-05-17 13:13:45 +0800

    chore: ruff format and check

--- blog/settings.py
+++ blog/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -74,7 +75,9 @@ WSGI_APPLICATION = "blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("DJANGO_DEFAULT_DB_ENGINE", "django.db.backends.sqlite3"),
+        "ENGINE": os.environ.get(
+            "DJANGO_DEFAULT_DB_ENGINE", "django.db.backends.sqlite3"
+        ),
         "NAME": os.environ.get("DJANGO_DEFAULT_DB_NAME", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("DJANGO_DEFAULT_DB_USER", ""),
         "PASSWORD": os.environ.get("DJANGO_DEFAULT_DB_PASSWORD", ""),
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
-bind = '0.0.0.0:8000'
-worker_class = 'uvicorn.workers.UvicornWorker'
+bind = "0.0.0.0:8000"
+worker_class = "uvicorn.workers.UvicornWorker"
 
 # https://docs.gunicorn.org/en/stable/settings.html#workers
 workers = multiprocessing.cpu_count() * 2 + 1
